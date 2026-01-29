1ed6462e4abb9fc7858db594f433c966c1e8b94f
Hussein <hussejnali3@gmail.com>
2026-01-30 00:29:36 +0600

    rewrite readme file

--- config.py
+++ config.py
@@ -1,19 +1,18 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+import os
 class Settings(BaseSettings):
     APP_NAME: str = "fastapi-service"
     DEBUG: bool = False
     
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
+    db_host: str = os.getenv("DB_HOST", "postgres")
+    db_port: int = int(os.getenv("DB_PORT", "5432"))
+    db_name: str = os.getenv("DB_NAME", os.getenv("POSTGRES_DB", "mydb"))
+    db_user: str = os.getenv("DB_USER", os.getenv("POSTGRES_USER", "user"))
+    db_pass: str = os.getenv("DB_PASS", os.getenv("POSTGRES_PASSWORD", "password123"))
+    db_driver: str = "postgresql+asyncpg"
 
     @property
     def DATABASE_URL(self) -> str:
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_pass}@{self.db_host}:{self.db_port}/{self.db_name}"
 
 settings = Settings()
